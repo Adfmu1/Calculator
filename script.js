9da0e3d5f8f753aa7b4a13140a41680e9977fdef
Adfmu <Adfmu3@gmail.com>
2024-05-20 01:57:50 +0200

    create the operate function and variables for future actions

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+let firstNumber = 3;
+let secondNumber = 5;
+let operator = "+";
+
+function operate (firstNumber, secondNumber, operator) {
+    switch (operator) {
+        case "+":
+            add(firstNumber, secondNumber);
+        case "-":
+            subtract(firstNumber, secondNumber);
+        case "*":
+            multiply(firstNumber, secondNumber);
+        case "/":
+            divide(firstNumber, secondNumber);
+    }
+
+}
+
 function add (...arr) {
     return arr.reduce(
         (total, current) => total + current, 0,
